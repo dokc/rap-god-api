a3f38da960c084cffc29558174afb588a90767f9
Ayushman Mishra <ayushvidushi01@gmail.com>
2022-04-11 20:29:32 +0530

    changed name

--- src/models/RaptbModel.js
+++ src/models/RaptbModel.js
@@ -3,11 +3,14 @@ export default {
       video_link :     "text",
       lyrics     :     "text",
       label      :     "text",
-      date_of_release :"date",
+      date_of_release :{
+      type:"date",
+      default: {"$db_function": "toTimestamp(now())"}
+      },
       singer	   :   "text",
       miscell_inf  :   "text",
       lyricist_name :  "text", // creator of rap
       rap_id :         "uuid", // unique for each rap	
       },
-  key: [["rap_id"],"lyricist_name"]
+  key: ["rap_id","lyricist_name"]
 }
