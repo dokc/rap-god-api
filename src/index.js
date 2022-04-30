959173a00da7740b53a16d58546a904a164ce0e6
Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
2022-04-30 19:42:45 +0530

    Routers (#36)
    * Updates to strucutre
    
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
    
    * Updates to Jest
    
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
    
    * Patches to certain errors - Apollo.invalidschema
    
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
    
    * Update prettier.yml (#3)
    
    * Update prettier.yml
    
    * Addition of linted
    
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
    
    * Addition of linted
    
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
    
    * Update prettier.yml (#4)
    
    * Update prettier.yml
    
    * Addition of linted
    
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
    
    * Addition of linted
    
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
    
    * Prettified Code!
    
    Co-authored-by: AbhijithGanesh <AbhijithGanesh@users.noreply.github.com>
    
    * Updates
    
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
    
    * Changes
    
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
    
    * Changes
    
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
    
    * Changes
    
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
    
    * Addition of new routing, etc
    
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
    
    Co-authored-by: AbhijithGanesh <AbhijithGanesh@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,9 +1,44 @@
+var models = require("express-cassandra")
 import express from "express"
 import helmet from "helmet"
+import { setDirectory, consistencies } from "express-cassandra"
+import singer_router from "./routes/router"
 
 const app = express()
 
 const port = 3000
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+app.use(express.json())
+
+setDirectory(__dirname + "/models").bind(
+  {
+    clientOptions: {
+      contactPoints: ["127.0.0.1"],
+      localDataCenter: "datacenter1",
+      protocolOptions: { port: 9042 },
+      keyspace: "rapLyrics",
+      queryOptions: { consistency: consistencies.one },
+      socketOptions: { readTimeout: 0 },
+    },
+    ormOptions: {
+      defaultReplicationStrategy: {
+        class: "SimpleStrategy",
+        replication_factor: 1,
+      },
+      migration: "safe",
+    },
+  },
+  (err) => {
+    if (err) throw err
+    else {
+      console.log("Database successfully connected at port 9042")
+    }
+  }
+)
 
 app.get("env")
 // Using Helmet for CORS and other cross-origin security policy
@@ -18,8 +53,8 @@ app.get("/", (req, res) => {
   res.send("Hello World!")
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
+app.use("/singer", singer_router)
+
 
 export default app
